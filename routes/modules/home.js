c361897883463d6a031f2eb36d9ab6f091ba6dd6
Ashley <ahappyday4all@gmail.com>
2021-05-20 00:03:22 +0800

    feat: show all the Url on home page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const { find } = require('../../models/url')
 const Url = require('../../models/url')
 const shrinkUrl = require('../../public/javascripts/shrinkUrl')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('index')
+  return Url.find()
+    .lean()
+    .then(url => {
+      res.render('index', { url })
+    })
 })
 
 router.post('/shortUrls', async (req, res) => {
