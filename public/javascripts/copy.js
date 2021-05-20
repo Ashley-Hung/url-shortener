a552f1b18ef9dd6e8ff1295be9ea9c6a67778683
Ashley <ahappyday4all@gmail.com>
2021-05-20 15:46:45 +0800

    feat: modify copy function

--- public/javascripts/copy.js
+++ public/javascripts/copy.js
@@ -1,9 +1,9 @@
 /* Copy URL function */
 function copyUrl() {
   const copyUrl = document.querySelector('#shortUrl')
-  copyUrl.select()
-  document.execCommand('copy')
-  alert(`Copied the URL：${copyUrl.value}`)
+  const clipboard = navigator.clipboard
+
+  clipboard.writeText(copyUrl.value).then(() => alert(`Copied the URL：${copyUrl.value}`))
 }
 
 module.exports = copyUrl
