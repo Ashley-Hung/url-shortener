2bb2cd0c3b62327d93dacba2ee64a39ded84ee3f
Ashley <ahappyday4all@gmail.com>
2021-05-20 23:57:59 +0800

    feat: modify function to prevent copying empty

--- public/javascripts/copy.js
+++ public/javascripts/copy.js
@@ -3,7 +3,9 @@ function copyUrl() {
   const copyUrl = document.querySelector('#shortUrl')
   const clipboard = navigator.clipboard
 
-  clipboard.writeText(copyUrl.value).then(() => alert(`Copied the URL：${copyUrl.value}`))
+  if (copyUrl.value) {
+    clipboard.writeText(copyUrl.value).then(() => alert(`Copied the URL：${copyUrl.value}`))
+  }
 }
 
 module.exports = copyUrl
